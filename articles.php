2dceaf48d9b0a76b81d88bea8b8adf3bd5b5800a
Glodi Mietete <glodimietete@gmail.com>
2025-03-02 19:40:46 +0100

    Opération READ (CRUD) affichage de données venant de la base de donées dans la datatable  pour l'article

--- articles.php
+++ articles.php
@@ -4,18 +4,32 @@ include_once 'main.php';
 ?>
 
 <h1 class="mt-5">Articles</h1>
+
+
+<?php
+$query = "SELECT * FROM article";
+$pdostmt = $pdo->prepare($query);
+$pdostmt->execute();
+?>
 <table id="myTable" class="display">
     <thead>
         <tr>
-            <th></th>
-            <th></th>
+            <th>ID</th>
+            <th>DESCRIPTION</th>
+            <th>PRIX UNITAIRE</th>
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td></td>
-            <td></td>
-        </tr>
+        <?php
+        while ($row = $pdostmt->fetch(PDO::FETCH_ASSOC)) :
+        ?>
+            <tr>
+                <td><?php echo $row['idarticle']; ?></td>
+                <td><?php echo $row['description']; ?></td>
+                <td><?php echo $row['prix_unitaire']; ?></td>
+            </tr>
+
+        <?php endwhile; ?>
 
     </tbody>
 </table>
