1d093b19d1fe37cf867b7158755d138c34d9090c
Glodi Mietete <glodimietete@gmail.com>
2025-03-08 15:47:43 +0100

    Opération CRUD READ (Select) dans la table Commande vers la datatable

--- commandes.php
+++ commandes.php
@@ -1,22 +1,51 @@
 <?php
 include_once 'header.php';
 include 'main.php';
+$count = 0;
+$liste = [];
+$query = "SELECT idcommande FROM commande WHERE idcommande IN (SELECT commande_id FROM ligne_de_commande)";
+$pdostmt = $pdo->prepare($query);
+$pdostmt->execute();
+
+foreach ($pdostmt->fetchAll(PDO::FETCH_NUM) as $tabvalues) {
+
+    foreach ($tabvalues as $value) {
+        $liste[] = $value;
+    }
+}
 ?>
 
 
 <h1 class="mt-5">Commandes</h1>
+
+
+<?php
+$query = "SELECT * FROM commande";
+$pdostmt = $pdo->prepare($query);
+$pdostmt->execute();
+?>
 <table id="myTable" class="display">
     <thead>
         <tr>
-            <th></th>
-            <th></th>
+            <th>ID COMMANDE</th>
+            <th> ID CLIENT</th>
+            <th>DATE</th>
+            <th>ACTIONS</th>
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td></td>
-            <td></td>
-        </tr>
+
+        <?php while ($row = $pdostmt->fetch(PDO::FETCH_ASSOC)) :
+            $count++;
+        ?>
+            <tr>
+                <td><?php echo $row['idcommande']; ?></td>
+                <td><?php echo $row['idclient']; ?></td>
+                <td><?php echo $row['date']; ?></td>
+                <td></td>
+            </tr>
+
+        <?php endwhile; ?>
 
     </tbody>
 </table>
