46a8ad95ee99daab5c5876c19bf6d1332d5900a5
Glodi Mietete <glodimietete@gmail.com>
2025-03-08 17:51:43 +0100

    Opération CRUD (CREAT) ajout d'une commnade

--- addCommande.php
+++ addCommande.php
@@ -6,9 +6,24 @@ $query = "SELECT idclient FROM client";
 $pdostmt = $pdo->prepare($query);
 $pdostmt->execute();
 
+
+
+// Ajouter une commande
+
+if (!empty($_POST["idClient"]) &&  !empty($_POST["date"])) {
+    $idClient = $_POST["idClient"];
+    $date = $_POST["date"];
+    $query = "INSERT INTO commande (idclient, date) VALUES (:idClient, :date)";
+    $pdostmt = $pdo->prepare($query);
+    $pdostmt->execute([
+        ':idClient' => $idClient,
+        ':date' => $date
+    ]);
+    header("Location: commandes.php");
+    exit;
+}
+
 ?>
-
-
 
 
 <h1 class="mt-5 ml-5">Ajout une commande</h1>
