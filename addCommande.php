bd4d9102c35b4e019cb0299f9c8607cb518e1aae
Glodi Mietete <glodimietete@gmail.com>
2025-03-08 17:08:15 +0100

    Affichage des id Client dans le selecteur

---
+++ addCommande.php
@@ -0,0 +1,50 @@
+<?php
+ob_start();
+include_once 'header.php';
+include 'main.php';
+$query = "SELECT idclient FROM client";
+$pdostmt = $pdo->prepare($query);
+$pdostmt->execute();
+
+?>
+
+
+
+
+<h1 class="mt-5 ml-5">Ajout une commande</h1>
+
+
+<form class="row g-3" method="POST">
+
+    <div class="col-md-6">
+        <label for="inputIdClient" class="form-label">ID CLIENT</label>
+        <select class="form-control" name="idClient" id="inputIdClient" required>
+            <?php
+
+            foreach ($pdostmt->fetchAll(PDO::FETCH_NUM) as $tabvalues) {
+
+                foreach ($tabvalues as $value) {
+                    echo "<option value='$value'>$value</option>";
+                }
+            }
+            ?>
+        </select>
+    </div>
+    <div class="col-md-6">
+        <label for="inputDate" class="form-label">DATE</label>
+        <input type="date" class="form-control" id="inputDate" name="date" required>
+    </div>
+
+    <div class="col-12">
+        <button type="submit" class="btn btn-primary">Ajouter</button>
+    </div>
+</form>
+
+
+</div>
+</main>
+
+
+<?php
+include_once 'footer.php';
+?>
