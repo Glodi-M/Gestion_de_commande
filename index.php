aae3ed1cec6cce515139cadc16dda9f70377a161
Glodi Mietete <glodimietete@gmail.com>
2025-03-10 14:13:59 +0100

    OPERATION CRUD (READ) Affichage de la jointure des tables dans la page d'accueil

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
 include_once 'header.php';
 include_once 'main.php';
+
+$query = "SELECT 
+c.nom, 
+c.ville, 
+c.telephone, 
+co.date, 
+a.description, 
+a.prix_unitaire, 
+lc.quantite
+FROM 
+client c
+JOIN 
+commande co ON c.idclient = co.idclient
+JOIN 
+ligne_de_commande lc ON co.idcommande = lc.commande_id
+JOIN 
+article a ON lc.article_id = a.idarticle";
+
+$objstmt = $pdo->prepare($query);
+$objstmt->execute();
+// $objstmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 <!-- Begin page content -->
@@ -10,16 +32,32 @@ include_once 'main.php';
 <table id="myTable" class="display">
     <thead>
         <tr>
-            <th></th>
-            <th></th>
+            <th>NOM</th>
+            <th>TELEPHONE</th>
+            <th>VILLE</th>
+            <th>DATE</th>
+            <th>DESCRIPTION</th>
+            <th>PRIX UNITAIRE</th>
+            <th>QUANTITE</th>
+
+
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td></td>
-            <td></td>
-        </tr>
 
+        <?php while ($row = $objstmt->fetch(PDO::FETCH_ASSOC)) : ?>
+            <tr>
+
+                <td><?php echo $row['nom']; ?></td>
+                <td><?php echo $row['telephone']; ?></td>
+                <td><?php echo $row['ville']; ?></td>
+                <td><?php echo $row['date']; ?></td>
+                <td><?php echo $row['description']; ?></td>
+                <td><?php echo $row['prix_unitaire']; ?></td>
+                <td><?php echo $row['quantite']; ?></td>
+
+            </tr>
+        <?php endwhile; ?>
     </tbody>
 </table>
 </div>
@@ -29,5 +67,6 @@ include_once 'main.php';
 
 
 <?php
+$objstmt->closeCursor();
 include_once 'footer.php';
 ?>
